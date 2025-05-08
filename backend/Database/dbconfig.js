f1caa09d97469f7e3e1a6ffd6e9511d72f73738b
Md Ghouse <tomdghouse@gmail.com>
2025-05-08 16:28:41 +0530

    Ghouse first commit

--- backend/Database/dbconfig.js
+++ backend/Database/dbconfig.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-//import mongoose from "mongoose"
 const Database = async () => {
     try {
         await mongoose.connect(`mongodb+srv://musharaf:Skmusharaf13@cluster0.exvhe.mongodb.net/blog-system?retryWrites=true&w=majority&appName=Cluster0/ecommerce`);
